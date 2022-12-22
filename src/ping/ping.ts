76d84181f1b8ca1183a77bb6efcaa97ced87a642
Dmitry Marchuk <code@dmarchuk.com>
2022-12-22 16:21:01 +0200

    Relax ping timeouts

--- src/ping/ping.ts
+++ src/ping/ping.ts
@@ -3,8 +3,8 @@ import ping from "ping";
 
 export async function pingHost(
   host: string,
-  deadline = 5,
-  timeout = 1,
+  deadline = 10,
+  timeout = 3,
 ): Promise<string | undefined> {
   const numeric = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(host);
 
@@ -19,13 +19,26 @@ export async function pingHost(
   return undefined;
 }
 
-export async function tcpPingHost(host: string, port: number): Promise<boolean> {
+export async function tcpPingHost(
+  host: string,
+  port: number,
+  timeout = 3000,
+  attempts = 3,
+): Promise<boolean> {
   try {
     return new Promise<boolean>((resolve, reject) => {
-      tcpp.probe(host, port, (err, res) => {
-        if (err) reject(err);
-        else resolve(res);
-      });
+      tcpp.ping(
+        {
+          address: host,
+          port,
+          timeout,
+          attempts,
+        },
+        (err, res) => {
+          if (err) reject(err);
+          else resolve(res.min !== undefined);
+        },
+      );
     });
   } catch (err) {
     return Promise.resolve(false);
