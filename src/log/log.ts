6233655bc72439d606ce7f05b8faf8e68d4323c2
Dmitry Marchuk <code@dmarchuk.com>
2022-12-23 21:27:53 +0200

    start from scratch

--- src/log/log.ts
+++
@@ -1,7 +0,0 @@
-import winston from "winston";
-
-export const log = winston.createLogger({
-  level: process.env["NODE_ENV"] !== "production" ? "debug" : "info",
-  format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-  transports: [new winston.transports.Console()],
-});
--- src/stats/eventStats.ts
+++
@@ -1,9 +0,0 @@
-import { Stats } from "../types";
-import { Config } from "../config/Config";
-import { getSingleEventStats } from "./singleEventStats";
-import { getSummaryStats } from "./summaryStats";
-import { Event } from "../events/Event";
-
-export function getStatsForEvent(config: Config, event: Event): Stats[] {
-  return [getSingleEventStats(event, config), ...getSummaryStats(event, config)];
-}
